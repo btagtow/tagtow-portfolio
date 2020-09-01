a7a5a0d2b24dc55e8a65a5bcb52fe1bc175d7784
Ben Tagtow <bentagtow@gmail.com>
2020-09-01 14:31:19 -0600

    switching branches - precommit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,17 +12,22 @@ export default function intro() {
             </p>
             <p>
             My background is in marketing & communications, which was my career until last year. 
-            However, a chance-encounter with a neighbor last summer (who happened to be a software engineer) 
+            However, a chance-encounter with a neighbor last summer who happened to be a software engineer
             led all of that to change. 
             </p>
             <p>
             After discovering the possibilities (along with awesome challenges) of software development, 
             I realized I had been missing an important sense of fulfillment in my life and I needed to pursue it. 
             </p>
+            <p className="emphasis">
+            The opportunity to change the world through technology has never been more potent. 
+            </p>
             <p>
+            
             That's why, in 2019, I quit my job and 
             headed to coding school. And I've never looked back since. 
             </p>
+            
             <p>
 
             If you'd like to get to know me, you can find my professional history 
